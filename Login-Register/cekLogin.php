90b95940688fee56726b42e4521be10cac7ea739
SparkyCloudy <satriagama1@gmail.com>
2023-05-31 01:00:59 +0800

    fixed login checker

--- Login-Register/cekLogin.php
+++ Login-Register/cekLogin.php
@@ -7,16 +7,22 @@
     <title>Check</title>
 </head>
 <body>
+    <?php
     session_start();
+    include 'connection.php';
 
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if (nanti diisi){
+    $data = mysqli_query($koneksi, "SELECT * FROM user WHERE `username` = $username AND `password` = $password");
+    $role = mysqli_fetch_array($data);
+
+    if (isset($role)){
         $_SESSION['username'] = $username;
-        header("location:LoginForm.php?pesan=login berhasil&role={$role['role']}")
+        header("location:LoginForm.php?pesan=loginberhasil&role={$role['role']}");
     } else {
-        header ("location:LoginForm.php?pesan=gagal")
+        header ("location:LoginForm.php?pesan=gagal");
     }
+?>
 </body>
 </html>
