7b5a1acc3e7137004f6cb59187f58b10bf8188e6
Faradaii <kdkfrdy@gmail.com>
2023-06-01 06:49:03 +0000

    refactor: refactoring connection.php

--- Login-Register/connection.php
+++ Login-Register/connection.php
@@ -1,7 +1,7 @@
 <?php 
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "";
+$db_pass = "pwduas123";
 $db_name = "mindfulspace";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
