93c3e9bda37439b18e7ced424974149b3ee05352
Faraday Kadek <114233356+Faradaii@users.noreply.github.com>
2023-06-23 11:30:17 +0800

    fix: fix the bug for pagination and search also improve showing data

--- Dashboard-Administrator/editUser.php
+++ Dashboard-Administrator/editUser.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="styling/nav-style.css">
+    <link rel="stylesheet" href="styling/update-style.css">
+    <link rel="stylesheet" href="styling/style.css">
+    <link rel="stylesheet" href="styling/dropdown-style.css">
+    <link rel="stylesheet" href="styling/main-style.css">
 </head>
 <body>
 <?php
@@ -17,46 +24,60 @@
     
     $result = mysqli_fetch_array($data);
 ?>
+    
 <form action = "editUserAction.php" method = "post">
-    <table>
-        <tr>
-            <td><input type="hidden" name="id" value="<?php echo $result['id']; ?>"></td>
-        </tr>
-        <tr>
-            <td>Username</td>
-            <td><input type ="text" name = "username" value="<?php echo $result['username']; ?>"></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-            <td><input type ="text" name = "password" value="<?php echo $result['password']; ?>"></td>
-        </tr>
-        <tr>
-            <td>Role</td>
-            <td>
-                <select type ="text" name = "role" value="<?php echo $result['role']; ?>">
-                    <?php 
-                        if ($result['role'] == 'dokter') {?>
-                        <option value="dokter" selected>Dokter</option>
-                        <option value="user" >User</option>
-                        <?php 
-                        } else {?>
-                        <option value="user" selected>User</option>
-                        <option value="dokter" >Dokter</option>
-                        <?php 
-                        }?>
-                    ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-        <td>
-            <button type = "submit">Edit User</button>
-        </td>
-        </tr>
-    <table>
-</form>
-<?php
+<div class="container">
+<input type="hidden" name="id" value="<?php echo $result['id']; ?>">
+        <div class="left">
+            <span style="margin-top: 0.6rem;"></span>
+            <div>
+                <p>Username</p>
+            </div>
+            <div>
+                <p>Password</p>
+            </div>
+            <div>
+                <p>Role</p>
+            </div>
+            <div>
+            <form action="Dashboard.php">
 
-?>
+            <button class="back">
+                <i class="fa-solid fa-arrow-left" style="margin-inline: 5%;"></i>
+                Back
+            </button>
+
+            </form>
+            </div>
+        </div>
+        <div class="right">
+            <div>
+            <input type ="text" name = "username" value="<?php echo $result['username']; ?>">
+            </div>
+            <div>
+            <input type ="text" name = "password" value="<?php echo $result['password']; ?>">
+            </div>
+            <div>
+            <select type ="text" name = "role" value="<?php echo $result['role']; ?>" id="inputbutton">
+                <?php 
+                    if ($result['role'] == 'dokter') {?>
+                    <option value="dokter" selected>Dokter</option>
+                    <option value="user" >User</option>
+                    <?php 
+                    } else {?>
+                    <option value="user" selected>User</option>
+                    <option value="dokter" >Dokter</option>
+                    <?php 
+                    }?>
+                ?>
+            </select>
+            </div>
+            <div class="editdiv">
+            <button type = "submit" class="editbutton">Edit User</button>
+            </div>
+        </div>
+</div>
+</form>
+
 </body>
 </html>
--- Dashboard-Administrator/styling/script.js
+++ Dashboard-Administrator/styling/script.js
@@ -2,39 +2,73 @@
  * TOLONG JANGAN DIUBAH - UBAH
  */
 
-const currentChecked = localStorage.getItem('checkedInput')
+// const currentChecked = localStorage.getItem('checkedInput')
 const radio = document.querySelectorAll('input[name=total]')
 const label = document.querySelectorAll('.radio__label')
-const defaultCheckedLabel = document.querySelector('label[for='+ currentChecked +']')
+// const defaultCheckedLabel = document.querySelector('input[name=total].checked ')
 
-defaultCheckedLabel.style.backgroundColor = 'var(--main-blue)'
-defaultCheckedLabel.style.color = 'white'
-defaultCheckedLabel.style.boxShadow = '0 0 10px 1px var(--main-blue)'
+// defaultCheckedLabel.style.backgroundColor = 'var(--main-blue)'
+// defaultCheckedLabel.style.color = 'white'
+// defaultCheckedLabel.style.boxShadow = '0 0 10px 1px var(--main-blue)'
 
 // Iterasi ke setiap input type=radio name=total
-for (let i = 0; i < radio.length; i++) {
+// for (let i = 0; i < radio.length; i++) {
 
-    radio[i].addEventListener('change', function() {
+//     radio[i].addEventListener('change', function() {
 
-        if (this.checked) {
+//         if (this.checked) {
 
-            console.log('check')
+//             console.log('check')
             
-            // Mengganti label selain label[i] seperti styling semula
-            for (let j = 0; j < radio.length; j++) {
-                if (j !== i) {
-                    label[j].style.backgroundColor = ''
-                    label[j].style.color = ''
-                    label[j].style.boxShadow = ''
-                }
-            }
+//             // Mengganti label selain label[i] seperti styling semula
+//             for (let j = 0; j < radio.length; j++) {
+//                 if (j !== i) {
+//                     label[j].style.backgroundColor = ''
+//                     label[j].style.color = ''
+//                     label[j].style.boxShadow = ''
+//                 }
+//             }
             
-            label[i].style.backgroundColor = 'var(--main-blue)'
-            label[i].style.color = 'white'
-            label[i].style.boxShadow = '0 0 10px 1px var(--main-blue)'
+//             label[i].style.backgroundColor = 'var(--main-blue)'
+//             label[i].style.color = 'white'
+//             label[i].style.boxShadow = '0 0 10px 1px var(--main-blue)'
 
-        }
+//         }
 
-    })
+//     })
+
+// }
+
+const radioUser = document.querySelector("#total__user");
+const radioDokter = document.querySelector("#total__dokter");
+const labelUser = document.querySelector("#label__user");
+const labelDokter = document.querySelector("#label__dokter");
+
+function changecolor(a, b){
+    a.style.backgroundColor = 'var(--main-blue)'
+    a.style.color = 'white'
+    a.style.boxShadow = '0 0 10px 1px var(--main-blue)'
+    b.style.backgroundColor = ''
+    b.style.color = ''
+    b.style.boxShadow = ''
 
 }
+
+const currentUrl = window.location.href;
+console.log(currentUrl)
+const params = new URLSearchParams(window.location.search);
+
+if (params.get("show") == 'user') {
+    changecolor(labelUser, labelDokter);
+} else if (params.get("show") == 'dokter'){
+    changecolor(labelDokter, labelUser);
+}
+
+
+radioDokter.addEventListener("change", () => {
+    window.location = window.location.pathname + "?show=dokter";
+});
+
+radioUser.addEventListener("change", () => {
+    window.location = window.location.pathname + "?show=user";
+});
