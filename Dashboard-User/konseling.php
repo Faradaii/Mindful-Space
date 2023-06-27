fad610fbdb4ad69cf14871c632991e5e74cbdbd6
Faradaii B <kdkfrdy@gmail.com>
2023-06-27 18:39:57 +0800

    feat: feature konseling dengan memilih dokter dan pemilihan waktu lebih jelas

--- Dashboard-User/konseling.php
+++ Dashboard-User/konseling.php
@@ -16,6 +16,8 @@
     
 <?php
     session_start();
+    require_once "../Helper/ConnectionUtil.php";
+    use Helper\ConnectionUtil;
     if(isset($_GET['message'])){
         $alertgagal = <<<JAVASCRIPT
         <script>
@@ -115,21 +117,54 @@
 
         <label for="keluhan">Keluhan</label>
         <input type="text" name="keluhan" id="keluhan">
-
+        
         <br><br><br>
-
-        <label for="waktu">Waktu</label>
-        <select name="waktu" id="waktu">
+        
+        <label for="dokter">Pilih Dokter</label>
+        <select name="dokter" id="dokter" required>
+            <option selected value="">Silahkan Pilih Dokter Anda</option>
             <?php 
-            for ($i = 8; $i <= 22; $i++) {
-                $d = strtotime("$i:00");
-                $time = date("H", $d);
+            $queryShowAvailableDokters = <<<SQL
+                SELECT identitas.namalengkap, dokters.* FROM `dokters` JOIN identitas ON dokters.id_dokter = identitas.id_user
+            SQL;
+            $queryShowDokterName = <<<SQL
+                SELECT namalengkap, id_user FROM `identitas`
+            SQL;
+            $data = mysqli_query(ConnectionUtil::connect(), $queryShowAvailableDokters);
+            while ($result = mysqli_fetch_array($data)) {
                 echo <<<HTML
-                <option value="$i">$time.00</option>
+                <option value="$result[id_dokter]">$result[namalengkap]</option>
                 HTML;
             }
             ?>
         </select>
+
+        <br><br><br>
+
+        <label for="waktu">Waktu</label>
+        <select name="waktu" id="waktu" disabled required>
+            <script>
+                let pilihdokter = document.querySelector("#dokter");
+                let waktu = document.querySelector("#waktu");
+                pilihdokter.addEventListener("change", function(){
+                    if (!pilihdokter.value == "") {
+                        waktu.disabled = false;
+                        var xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                waktu.innerHTML = (this.responseText);
+                            }
+                        };
+                        xhttp.open("GET", "scriptDapatkanWaktuDokter.php?dapatkanWaktuDokter="+pilihdokter.value, true);
+                        xhttp.send();
+                    } else {
+                        waktu.value = "";
+                        waktu.disabled = true;
+                    }
+                });
+            </script>
+        </select>
+        <small>*beberapa waktu mungkin tidak tersedia</small>
         
         <br><br><br><br><br>
 
@@ -137,7 +172,7 @@
 
             <a href="dashboard.php">Batal</a>    
 
-            <input type="submit" value="Cari Dokter">
+            <input type="submit" value="Booking">
 
         </div>
         
--- Dashboard-User/proseskonseling.php
+++ Dashboard-User/proseskonseling.php
@@ -7,24 +7,7 @@ $myid = $_SESSION['id'];
 
 $keluhan=$_POST['keluhan'];
 $waktu=intval($_POST['waktu']);
-
-$queryStatusDokter = <<<SQL
-        SELECT id_dokter FROM dokters WHERE status = '1'
-    SQL;
-
-$data = mysqli_query(ConnectionUtil::connect(), $queryStatusDokter);
-$result = mysqli_fetch_all($data);
-// Check kondisi jika dokter available tidak tersedia
-if(mysqli_num_rows($data) < 1){
-    header('location:konseling.php?message=gagal');
-} 
-
-else 
-{
-// Jika dokter available tersedia maka true kanjut
-$randomNumber = rand(0, sizeof($result)-1);
-$id_dokter = $result[$randomNumber][0];
-print_r($id_dokter);
+$id_dokter = $_POST['dokter'];
 
 $_SESSION['waktukonsul'] = $waktu;
 $_SESSION['id_from'] = $id_dokter;
@@ -34,7 +17,7 @@ $queryInsertAntrian = <<<SQL
 SQL;
 mysqli_query(ConnectionUtil::connect(), $queryInsertAntrian);
 header('location:konseling.php?message=sukses');
-}
+
 
 
 
